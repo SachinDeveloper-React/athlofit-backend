0610cb4fb01b185467cfd186efe535dbd1204498
Sachin <sachin@Sachins-MacBook-Pro.local>
2026-04-25 23:43:32 +0530

    google keys in env

--- src/config/firebase.admin.js
+++ src/config/firebase.admin.js
@@ -1,10 +1,13 @@
 // src/config/firebase.admin.js
-const admin = require('firebase-admin');
+const admin = require("firebase-admin");
 
 if (!admin.apps.length) {
-  const serviceAccount = require('./serviceAccountKey.json');
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+    credential: admin.credential.cert({
+      clientEmail: process.env.CLIENT_EMAIL,
+      privateKey: process.env.PRIVATE_KEY,
+      projectId: process.env.PROJECT_ID,
+    }),
   });
 }
 
